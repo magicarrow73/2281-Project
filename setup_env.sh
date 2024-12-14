8983fcd71a8a5faca5a1cf5f8605fc43a014e644
Russell Li <rli42@holygpu8a11401.rc.fas.harvard.edu>
2024-12-14 00:20:51 -0500

    making sure everything works

---
+++ setup_env.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+
+ENV_NAME=2281-project-env
+REQUIREMENTS_FILE=requirements.txt
+
+conda create -n "$ENV_NAME" python=3.9 -y
+conda activate "$ENV_NAME"
+
+pip install -r "$REQUIREMENTS_FILE"
+pip install accelerate
+pip install --upgrade --force-reinstall "numpy<2.0"
+pip install --upgrade torch --index-url https://download.pytorch.org/whl/cu118
+pip install sentencepiece
+pip install bitsandbytes
+
+#huggingface token for llama
+pip install huggingface_hub
+read -sp "put your hf token here: " HF_TOKEN
+echo
+
+huggingface-cli login --token $HF_TOKEN
+
+echo "finished setting up conda env"
